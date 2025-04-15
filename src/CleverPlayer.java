f253333215690e3ea8510794f35cc714a119ef84
Ahmad Atamna <ahmed@cs.huji.ac.il>
2025-04-15 15:27:41 +0300

    little changes + docs

--- src/CleverPlayer.java
+++ src/CleverPlayer.java
@@ -1,84 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
+/**
+ * CleverPlayer has a middle strategy, it beats WhateverPlayer almost always.
+ * CleverPlayer implements Player interface.
+ */
 public class CleverPlayer implements Player {
 
     private static final Random rand = new Random();
 
+    /**
+     * Default constructor, creates the CleverPlayer and it's ready to use.
+     */
     public CleverPlayer() {}
 
+    /**
+     * CleverPlayer plays a turn using his middle strategy.
+     * @param board The board that CleverPlayer will mark his move on.
+     * @param mark The mark that CleverPlayer will use to mark his new move.
+     */
     @Override
     public void playTurn(Board board, Mark mark) {
-        Mark opp = (mark == Mark.X) ? Mark.O : Mark.X;
-
         for (int i = 0; i < board.getSize(); i++) {
             for (int j = 0; j < board.getSize(); j++) {
                 if (board.getMark(i, j) != Mark.BLANK) continue;
 
                 board.putMark(mark, i, j);
-                int score = longestStreak(board, mark);
-                board.putMark(Mark.BLANK, i, j);
+                int score = Utilites.longestStreak(board, mark);
 
                 // if score is at least 2, then it's good enough
                 if (score >= 2) {
-                    board.putMark(mark, i, j);
                     return;
                 }
+
+                // reset mark
+                board.putMark(Mark.BLANK, i, j);
             }
         }
 
-        // if we couldn't make a 2-length streak, we'll just go random
-        int random_row, random_col;
-        random_row = rand.nextInt(board.getSize());
-        random_col = rand.nextInt(board.getSize());
-        board.putMark(Mark.BLANK, random_row, random_col);
-    }
-
-    private static int longestStreak(Board board, Mark mark) {
-        int n = board.getSize();
-        int max = 0;
-
-        // Rows
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                count = (board.getMark(i, j) == mark) ? count + 1 : 0;
-                max = Math.max(max, count);
-            }
-        }
-
-        // Columns
-        for (int j = 0; j < n; j++) {
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                count = (board.getMark(i, j) == mark) ? count + 1 : 0;
-                max = Math.max(max, count);
-            }
-        }
-
-        // Diagonals (↘)
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                int count = 0;
-                for (int d = 0; r + d < n && c + d < n; d++) {
-                    count = (board.getMark(r + d, c + d) == mark) ? count + 1 : 0;
-                    max = Math.max(max, count);
-                }
-            }
-        }
-
-        // Diagonals (↙)
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                int count = 0;
-                for (int d = 0; r + d < n && c - d >= 0; d++) {
-                    count = (board.getMark(r + d, c - d) == mark) ? count + 1 : 0;
-                    max = Math.max(max, count);
-                }
-            }
-        }
-
-        return max;
+        // if no result found, mark a random coords
+        int[] randomEmptyCoords = Utilites.pickRandomBoardEmptyCoords(board);
+        board.putMark(mark, randomEmptyCoords[0], randomEmptyCoords[1]);
     }
 }
